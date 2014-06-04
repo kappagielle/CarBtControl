ef864b1cfd4ec84595fb50b0955300a7b0b13d57
gianluigi <gvkerma@gmail.com>
2014-06-04 17:27:06 +0200

    primo commit

---
+++ app/src/main/java/com/kappagielle/carbtcontrol/app/Bluetooth.java
@@ -0,0 +1,184 @@
+package com.kappagielle.carbtcontrol.app;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+
+
+public class Bluetooth{
+
+	public final static String TAG = "CAR CONTROL 1.0";
+	private final static String BT_NAME = "HC-05";
+
+	private static BluetoothAdapter btAdapter = null;
+	private BluetoothSocket btSocket = null;
+	private OutputStream outStream = null;
+	protected ConnectedThread mConnectedThread;
+
+	// SPP UUID service 
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+	protected final Handler mHandler;
+	public final static int BL_NOT_AVAILABLE = 1;        
+	public final static int BL_INCORRECT_ADDRESS = 2;
+	public final static int BL_REQUEST_ENABLE = 3;
+	public final static int BL_SOCKET_FAILED = 4;
+	public final static int RECIEVE_MESSAGE = 5;
+
+	Bluetooth(Context context, Handler handler){
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		mHandler = handler;
+		if (btAdapter == null) {
+			mHandler.sendEmptyMessage(BL_NOT_AVAILABLE);
+		}
+	}
+	
+	public String getAddress() {
+		
+		if(btAdapter != null) {
+			return btAdapter.getAddress();
+		} else {
+			return "";
+		}
+	}
+
+	public void checkBTState() {
+		if(btAdapter == null) { 
+			mHandler.sendEmptyMessage(BL_NOT_AVAILABLE);
+		} else {
+			if (btAdapter.isEnabled()) {
+				Log.d(TAG, "Bluetooth ON");
+			} else {
+				mHandler.sendEmptyMessage(BL_REQUEST_ENABLE);
+			}
+		}
+	}
+
+	public void btConnect() {   	
+		Log.d(TAG, "...On Resume...");
+			
+		BluetoothDevice device = null;
+		for(BluetoothDevice bd : btAdapter.getBondedDevices()) {
+			if(bd.getName().equals(BT_NAME)) {
+				device = bd;
+				break;
+			}
+		}
+		
+		try {
+			btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+		} catch (IOException e) {
+			Log.d(TAG, "In onResume() and socket create failed: " + e.getMessage());
+			mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+			return;
+		}
+
+		btAdapter.cancelDiscovery();
+		Log.d(TAG, "...Connecting...");
+		try {
+			btSocket.connect();
+			Log.d(TAG, "...Connection ok...");
+		} catch (IOException e) {
+			try {
+				btSocket.close();
+			} catch (IOException e2) {
+				Log.d(TAG, "In onResume() and unable to close socket during connection failure" + e2.getMessage());
+				mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+				return;
+			}
+		}
+
+		// Create a data stream so we can talk to server.
+		Log.d(TAG, "...Create Socket...");
+
+		try {
+			outStream = btSocket.getOutputStream();
+		} catch (IOException e) {
+			Log.d(TAG, "In onResume() and output stream creation failed:" + e.getMessage());
+			mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+			return;
+		}
+
+		mConnectedThread = new ConnectedThread();
+		mConnectedThread.start();
+	
+	}
+
+	public void BT_onPause() {
+		Log.d(TAG, "...On Pause...");
+		if (outStream != null) {
+			try {
+				outStream.flush();
+			} catch (IOException e) {
+				Log.d(TAG, "In onPause() and failed to flush output stream: " + e.getMessage());
+				mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+				return;
+			}
+		}
+
+		if (btSocket != null) {
+			try {
+				btSocket.close();
+			} catch (IOException e2) {
+				Log.d(TAG, "In onPause() and failed to close socket." + e2.getMessage());
+				mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+				//return;
+			}
+		}
+	}
+
+	public void sendData(String message) {
+		byte[] msgBuffer = message.getBytes();
+
+		Log.i(TAG, "Send data: " + message);
+
+		if (outStream != null) {
+			try {
+				outStream.write(msgBuffer);
+			} catch (IOException e) {
+				Log.d(TAG, "In onResume() and an exception occurred during write: " + e.getMessage());
+				mHandler.sendEmptyMessage(BL_SOCKET_FAILED);
+				//return;
+			}
+		} else Log.d(TAG, "Error Send data: outStream is Null");
+	}
+
+	private class ConnectedThread extends Thread {
+
+		private final InputStream mmInStream;
+
+		public ConnectedThread() {
+			InputStream tmpIn = null;
+			try {
+				tmpIn = btSocket.getInputStream();
+			} catch (IOException e) {
+            //
+            }
+
+			mmInStream = tmpIn;
+		}
+
+		public void run() {
+			byte[] buffer = new byte[256];
+			int bytes;
+
+			while (true) {
+				try {
+					bytes = mmInStream.read(buffer);
+					mHandler.obtainMessage(RECIEVE_MESSAGE, bytes, -1, buffer).sendToTarget();
+				} catch (IOException e) {
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ app/src/main/java/com/kappagielle/carbtcontrol/app/MyCar.java
@@ -0,0 +1,89 @@
+package com.kappagielle.carbtcontrol.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+
+public class MyCar extends ActionBarActivity implements SensorEventListener {
+
+    private Bluetooth bl = null;
+    private SensorManager manager;
+    private Sensor mAccel;
+
+    //inserisco un commento
+
+    private boolean faroAnteriore = false;
+    private boolean faroPosteriore = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_car);
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccel = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        ToggleButton accensione = (ToggleButton)findViewById(R.id.onOffBtn);
+        Button luciAnteriori = (Button)findViewById(R.id.luciAnterioriBtn);
+        Button luciPosteriori = (Button)findViewById(R.id.luciPosterioriBtn);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_car, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values;
+
+        Integer y = (int)values[SensorManager.AXIS_Y - 1];
+
+        //qui invio i dati via bluetouth:
+        //prima arrotondo i parametri
+
+        if(bl != null) {
+
+            if(y > 2) {
+                bl.sendData("1"); //cmd destra
+            }
+            if(y < -2) { //cmd sinistra
+                bl.sendData("2");
+            }
+            if(y >= -2 && y <= 2 ) { //cmd dritto
+                bl.sendData("0");
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
